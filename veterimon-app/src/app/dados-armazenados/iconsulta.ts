9441724be914fdd1642d3904c26dd48460ccbfc9
Livia <livsoaresribeiro@outlook.com>
2021-11-01 16:16:11 -0300

    Criando o service de armazenamento e a interface para modelagem dos dados

---
+++ veterimon-app/src/app/dados-armazenados/iconsulta.ts
@@ -0,0 +1,10 @@
+export interface IConsulta {
+    id?:number
+    nome: string;
+    especie: string;
+    especialidade: string
+    data: string;
+    horario: number;
+    observacoes: string; 
+    
+}
